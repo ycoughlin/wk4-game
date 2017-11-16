d7ce4ef44478214d1923d775ea4aaf0989615df6
ycoughlin <senshine.c@gmail.com>
2017-11-16 18:09:09 -0500

    second commit

---
+++ assets/js/game.js
@@ -0,0 +1,26 @@
+// jquery
+$(document).ready(function(){
+
+// Variables
+
+
+var randomNum = [];
+var wins = 0;
+var losses = 0;
+var redCrystal = 5;
+var blueCrystal = 3;
+var yellowCrystal = 1;
+var greenCrystal = 10;
+
+
+
+// randomizer
+// var randomNumber = numbers[Math.floor(Math.random)*numbers.length];
+// console.log("initial number " + randomNumber);
+
+var random=[Math.floor(Math.random)*101*19];
+$('#randomNum').text(random);
+
+// Functions
+
+});
